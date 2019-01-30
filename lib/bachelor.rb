703fa8fc5421d702cc1c8bd6975f5471541c5a32
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-30 02:42:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant_hash|
     if contestant_hash["status"] == "winner"
-    return contestant_hash["name"].split(" ").first 
+    return contestant_hash["name"].split(" ").first
 end
 
 def get_contestant_name(data, occupation)
